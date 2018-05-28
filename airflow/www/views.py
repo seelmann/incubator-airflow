1686952df51bc4bf15834f5dbf18ccd2e3717aec
Stefan Seelmann <mail@stefan-seelmann.de>
2018-05-28 21:17:36 +0200

    [AIRFLOW-2529] Improve graph view performance and usability
    Limit number of dag runs shown in drop down. Add base date
    and number of runs widgets known from other views which
    allows kind of paging through all dag runs.
    
    cherry-pick 5f79465bd8be8b18cce85a8d40a48fdc6b8a81cf

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -1258,8 +1258,9 @@ class Airflow(BaseView):
     @login_required
     @wwwutils.gzipped
     @wwwutils.action_logging
-    def graph(self):
-        session = settings.Session()
+    @provide_session
+    def graph(self, session=None):
+        default_dag_run = 25
         dag_id = request.args.get('dag_id')
         blur = conf.getboolean('webserver', 'demo_mode')
         dag = dagbag.get_dag(dag_id)
@@ -1307,11 +1308,26 @@ class Airflow(BaseView):
         else:
             dttm = dag.latest_execution_date or datetime.utcnow().date()
 
+        base_date = request.args.get('base_date')
+        if base_date:
+            base_date = dateutil.parser.parse(base_date)
+        else:
+            # The DateTimeField widget truncates milliseconds and would loose
+            # the first dag run. Round to next second.
+            base_date = (dttm + timedelta(seconds=1)).replace(microsecond=0)
+
+        num_runs = request.args.get('num_runs')
+        num_runs = int(num_runs) if num_runs else default_dag_run
+
         DR = models.DagRun
         drs = (
             session.query(DR)
-                .filter_by(dag_id=dag_id)
-                .order_by(desc(DR.execution_date)).all()
+            .filter(
+                DR.dag_id == dag.dag_id,
+                DR.execution_date <= base_date)
+            .order_by(desc(DR.execution_date))
+            .limit(num_runs)
+            .all()
         )
         dr_choices = []
         dr_state = None
@@ -1320,7 +1336,13 @@ class Airflow(BaseView):
             if dttm == dr.execution_date:
                 dr_state = dr.state
 
-        class GraphForm(Form):
+        # Happens if base_date was changed and the selected dag run is not in result
+        if not dr_state and drs:
+            dr = drs[0]
+            dttm = dr.execution_date
+            dr_state = dr.state
+
+        class GraphForm(DateTimeWithNumRunsForm):
             execution_date = SelectField("DAG run", choices=dr_choices)
             arrange = SelectField("Layout", choices=(
                 ('LR', "Left->Right"),
@@ -1330,7 +1352,10 @@ class Airflow(BaseView):
             ))
 
         form = GraphForm(
-            data={'execution_date': dttm.isoformat(), 'arrange': arrange})
+            data={'execution_date': dttm.isoformat(),
+                  'arrange': arrange,
+                  'base_date': base_date,
+                  'num_runs': num_runs})
 
         task_instances = {
             ti.task_id: alchemy_to_dict(ti)
