f35c098446236714a6d54c53b35cc7f9424e3686
Stefan Seelmann <mail@stefan-seelmann.de>
2018-06-10 12:57:20 +0200

    Show failed task attempts in Gantt view

--- airflow/models.py
+++ airflow/models.py
@@ -1884,9 +1884,10 @@ class TaskFail(Base):
 
     __tablename__ = "task_fail"
 
-    task_id = Column(String(ID_LEN), primary_key=True)
-    dag_id = Column(String(ID_LEN), primary_key=True)
-    execution_date = Column(DateTime, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    task_id = Column(String(ID_LEN))
+    dag_id = Column(String(ID_LEN))
+    execution_date = Column(DateTime)
     start_date = Column(DateTime)
     end_date = Column(DateTime)
     duration = Column(Float)
